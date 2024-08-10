4daee5645f9598ebe72cecbeed67e2645301a34f
rabinam24 <rabinbhandari070@gmail.com>
2024-08-10 15:30:32 +0545

    hoho

--- frontend/src/auth0-config.js
+++ frontend/src/auth0-config.js
@@ -1,11 +1,6 @@
 export const auth0Config = {
-<<<<<<< HEAD
   domain: "accounts.google.com/o/oauth2/auth",
   clientId: "607168653915-f5sac4tb4mvuslkj2l0cit912nupdkr3.apps.googleusercontent.com",
   redirectUri: "https://740b-45-64-160-84.ngrok-free.app/callback"
-=======
-  domain: "oauth-staging.wlink.com.np",
-  clientId: "pole-finder",
-  redirectUri: "http://pole-finder.wlink.com.np:5173"
->>>>>>> origin/main
+
 };
